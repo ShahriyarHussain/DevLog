83a63696cdd9a678484cf17c5db7f0727aa979d8
MD. SHAHRIYAR HOSSAIN <shossainshihab36@keemail.me>
2021-01-20 00:46:40 +0600

    Added Dummy blog data (Video-3)

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,29 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts = [
+    {
+        'author': 'ShahriyarHossain',
+        'title': 'Blog Post 1',
+        'content': 'First post contents are here!',
+        'date_posted': 'January 20, 2020'
+    },
+    {
+        'author': 'ShonaK',
+        'title': 'Blog Post 2',
+        'content': '2nd post contents are here!',
+        'date_posted': 'January 19, 2020'
+    }
+]
+
 
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'blog/about.html')
 # Create your views here.
