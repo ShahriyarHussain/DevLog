29a495acf4a45bed2d20fa81adfa3662a1bf0d63
MD. SHAHRIYAR HOSSAIN <shossainshihab36@keemail.me>
2021-01-19 23:37:12 +0600

    Initial Commit

---
+++ blog/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def home(request):
+    return HttpResponse('<h1>Blog Home</h1>')
+
+
+def about(request):
+    return HttpResponse('<h1>Blog About</h1>')
+# Create your views here.
