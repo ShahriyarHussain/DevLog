797c069198a622f286d459cdd6220f3e995de13a
Muhammad Shahriyar Hossain <shahriyar@keemail.me>
2021-01-21 00:13:16 +0600

    Added Register Page

---
+++ users/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+# from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
+
+# Create your views here.
+
+
+def register(request):
+    form = UserCreationForm()
+    return render(request, 'users/register.html', {'form': form})
