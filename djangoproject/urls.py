a66c4844d08a1673366dd0fbef993d82005bce0e
Muhammad Shahriyar Hossain <shahriyar@keemail.me>
2021-01-22 19:42:11 +0600

    Update User profile page and now profile gets created automatically on registration

--- djangoproject/urls.py
+++ djangoproject/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import path, include
 from users import views as user_views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,3 +28,7 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('', include('blog.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
