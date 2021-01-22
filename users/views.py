1dab1a626454a29ab28b48430d932a44e171a104
ShahriyarHussain <shahriyar@keemail.me>
2021-01-23 01:49:32 +0600

    Provide users the option to edit their profiles

--- users/views.py
+++ users/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render, redirect
-# from django.http import HttpResponse
-# from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
-from .forms import UserRegistrationForm
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from .forms import UserRegistrationForm, UserUpdateForm, ProfileUpdateForm
+
 
 # Create your views here.
 
@@ -29,4 +29,26 @@ def register(request):
 
 @login_required
 def profile(request):
-    return render(request, 'users/profile.html')
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        p_form = ProfileUpdateForm(request.POST,
+                                   request.FILES,
+                                   instance=request.user.profile)
+
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(request, f'Profile Updated!')
+            return redirect('profile')
+        else:
+            messages.warning(request, f'No Changes Made')
+
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm()
+
+    context = {
+        'u_form': u_form,
+        'p_form': p_form
+    }
+    return render(request, 'users/profile.html', context)
