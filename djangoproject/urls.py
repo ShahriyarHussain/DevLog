46e0e4fae92f56bc200ebcd47c317d79d5a0535c
Muhammad Shahriyar Hossain <shahriyar@keemail.me>
2021-01-28 20:06:59 +0600

    Finalize Password Reset Feature

--- djangoproject/urls.py
+++ djangoproject/urls.py
@@ -41,6 +41,11 @@ urlpatterns = [
              template_name='users/password_reset_confirm.html'
          ),
          name='password_reset_confirm'),
+    path('password-reset-complete/',
+         auth_views.PasswordResetView.as_view(
+             template_name='users/password_reset_complete.html'
+         ),
+         name='password_reset_complete'),
     path('', include('blog.urls')),
 ]
 
