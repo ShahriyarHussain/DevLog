aebf4123e63378845ec750f98818aec71badb093
ShahriyarHossain <shahriyar@keemail.me>
2021-01-31 12:01:51 +0000

    Updated .gitignore

--- djangoproject/settings.py
+++ djangoproject/settings.py
@@ -145,4 +145,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'shossainshihab36@gmail.com'
-EMAIL_HOST_PASSWORD = 'dgppesaxadsemqed'
+EMAIL_HOST_PASSWORD = 'nadbebpoqxsvuges'
