c634aa129de1597322a02a6e79e9306156b48a54
ShahriyarHussain <shossainshihab36@outlook.com>
2021-02-13 14:02:53 +0600

    Revert "Revert "Add Feedback Form""
    This reverts commit 8afa698029c3ac2b32e4e09b9705738f27fff806.

--- report/views.py
+++ report/views.py
@@ -1,28 +1,32 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse, HttpResponseRedirect
-# from .models import *
-# from .forms import contact_form #NEEDS WORK HERE
+from .models import Feedback
+from .forms import FeedbackForm  # NEEDS WORK HERE
 from django.core.mail import send_mail, BadHeaderError
 from django.shortcuts import redirect
 
 
 def feedback(request):
     if request.method == 'POST':
-        form = contact_form()
+        form = FeedbackForm()
     else:
-        form = contact_form(request.POST)
+        form = FeedbackForm(request.POST)
         if form.is_valid():
-            email = form.cleaned_data['email']
-            name = form.cleaned_data['name']
+            # email = form.cleaned_data['email']
+            # name = form.cleaned_data['name']
             content = form.cleaned_data['content']
+            form.save()
             try:
-                send_mail(email, name, content, [
-                          'californiamikegreen@yahoo.com'])
+                send_mail('An Issue Occured', content, 'shossainshihab36@gmail.com', [
+                    'md.shahriyar.hossain@g.bracu.ac.bd'])
             except BadHeaderError:
                 return HttpResponse('Invalid header found.')
-            return redirect('success')
-    return render(request, "contact.html", {'form': form})
+            return redirect('feedback-success')
+    return render(request, "report/feedback.html", {
+        'form': form,
+        'title': 'Report A Problem'
+    })
 
 
 def success(request):
-    return HttpResponse('Success! Thank you for your message.')
+    return render(request, "report/success.html")
