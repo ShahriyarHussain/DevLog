6e6341cd8aabcb625f792cf7772a4e81f29b1683
ShahriyarHussain <shossainshihab36@gmail.com>
2021-07-16 12:50:07 +0600

    Prepare project for rebuild

--- djangoproject/settings.py
+++ djangoproject/settings.py
@@ -18,7 +18,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY_DJANGO_1')
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY_1')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
